2526ec3c17a688abc928d78937d4ce2ac9d02205
Rajan Shah <rajan.shah@solutionanalysts.com>
2017-02-20 12:31:59 +0530

    Update Podspec

--- CustomTextField/Classes/CustomTextField.swift
+++ CustomTextField/Classes/CustomTextField.swift
@@ -10,36 +10,33 @@ import Foundation
 
 public extension UITextField {
     
-    
-    public func shouldChangeValue(){
+    public func shouldChangeValue(cardNumberBuffer : String){
+        let  kAcceptSDKDemoCreditCardLengthPlusSpaces:Int = (TextFieldManager.shared.style.kAcceptSDKDemoCreditCardLength + 3)
+        let kAcceptSDKDemoCreditCardObscureLength:Int = (TextFieldManager.shared.style.kAcceptSDKDemoCreditCardLength - 4)
+        
         var value = String()
-        let kAcceptSDKDemoSpace:String = " "
         
-        let length = self.text?.characters.count
-        let kAcceptSDKDemoCreditCardLength:Int = 16
-        let kAcceptSDKDemoCreditCardLengthPlusSpaces:Int = (kAcceptSDKDemoCreditCardLength + 3)
+        let length = cardNumberBuffer.characters.count
         
-        let kAcceptSDKDemoCreditCardObscureLength:Int = (kAcceptSDKDemoCreditCardLength - 4)
-        
-        for (i, _) in (self.text?.characters.enumerated())! {
+        for (i, _) in cardNumberBuffer.characters.enumerated() {
             
             // Reveal only the last character.
-            if (length! <= kAcceptSDKDemoCreditCardObscureLength) {
-                if (i == (length! - 1)) {
-                    let charIndex = self.text?.index((self.text?.startIndex)!, offsetBy: i)
-                    let tempStr = String((self.text?.characters.suffix(from: charIndex!))!)
+            if (length <= kAcceptSDKDemoCreditCardObscureLength) {
+                if (i == (length - 1)) {
+                    let charIndex = cardNumberBuffer.index(cardNumberBuffer.startIndex, offsetBy: i)
+                    let tempStr = String(cardNumberBuffer.characters.suffix(from: charIndex))
                     //let singleCharacter = String(tempStr.characters.first)
                     
                     value = value + tempStr
                 } else {
-                    value = value + "●"
+                    value = value + TextFieldManager.shared.style.maskText
                 }
             } else {
-                if (i < 12) {
-                    value = value + "●"
+                if (i < kAcceptSDKDemoCreditCardObscureLength) {
+                    value = value + TextFieldManager.shared.style.maskText
                 } else {
-                    let charIndex = self.text?.index((self.text?.startIndex)!, offsetBy: i)
-                    let tempStr = String((self.text?.characters.suffix(from: charIndex!))!)
+                    let charIndex = cardNumberBuffer.index(cardNumberBuffer.startIndex, offsetBy: i)
+                    let tempStr = String(cardNumberBuffer.characters.suffix(from: charIndex))
                     //let singleCharacter = String(tempStr.characters.first)
                     //let singleCharacter = String(tempStr.characters.suffix(1))
                     
@@ -50,9 +47,62 @@ public extension UITextField {
             
             //After 4 characters add a space
             if (((i + 1) % 4 == 0) && (value.characters.count < kAcceptSDKDemoCreditCardLengthPlusSpaces)) {
-                value = value + kAcceptSDKDemoSpace
+                value = value + TextFieldManager.shared.style.kAcceptSDKDemoSpace
             }
-            
         }
+        self.text = value
     }
 }
+
+
+// MARK: - TextField Style
+
+/**
+ `ToastStyle` instances define the look and feel for toast views created via the
+ `makeToast` methods as well for toast views created directly with
+ `toastViewForMessage(message:title:image:style:)`.
+ 
+ @warning `ToastStyle` offers relatively simple styling options for the default
+ toast view. If you require a toast view with more complex UI, it probably makes more
+ sense to create your own custom UIView subclass and present it with the `showToast`
+ methods.
+ */
+public struct TextFieldStyle {
+    
+    public init() {}
+    
+    /**
+     The mask text. Default is "●".
+     */
+    public var maskText = "●"
+    
+    public var  kAcceptSDKDemoCreditCardLength:Int = 16
+    public var  kAcceptSDKDemoExpirationLength:Int = 4
+    public var  kAcceptSDKDemoExpirationMonthLength:Int = 2
+    public var  kAcceptSDKDemoExpirationYearLength:Int = 2
+    public var  kAcceptSDKDemoCVV2Length:Int = 4
+    
+    public var  kAcceptSDKDemoSpace:String = " "
+    public var  kAcceptSDKDemoSlash:String = "/"
+    
+}
+
+// MARK: - TextField Manager
+
+/**
+ `TextFieldManager` provides general configuration options for all TextField.
+ */
+public class TextFieldManager {
+    
+    /**
+     The `ToastManager` singleton instance.
+     */
+    public static let shared = TextFieldManager()
+    
+    /**
+     The shared style. Used whenever toastViewForMessage(message:title:image:style:) is called
+     with with a nil style.
+     */
+    public var style = TextFieldStyle()
+}
+
--- Example/CustomTextField/ViewController.swift
+++ Example/CustomTextField/ViewController.swift
@@ -8,17 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+let kAcceptSDKDemoCreditCardLength:Int = 16
+let kAcceptSDKDemoCreditCardLengthPlusSpaces:Int = (kAcceptSDKDemoCreditCardLength + 3)
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var txtField: UITextField!
+    var cardNumberBuffer : String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func isMaxLength(_ textField:UITextField) -> Bool {
+        var result = false
+        
+        if ((textField.text?.characters.count)! > kAcceptSDKDemoCreditCardLengthPlusSpaces)
+        {
+            result = true
+        }
+        
+        return result
+    }
+}
 
+extension ViewController : UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if (string.characters.count > 0)
+        {
+            if (self.isMaxLength(textField)) {
+                return false
+            }
+            
+            self.cardNumberBuffer = String(format: "%@%@", self.cardNumberBuffer, string)
+        }
+        else
+        {
+            if (self.cardNumberBuffer.characters.count > 1)
+            {
+                let length = self.cardNumberBuffer.characters.count-1
+                self.cardNumberBuffer = self.cardNumberBuffer[self.cardNumberBuffer.index(self.cardNumberBuffer.startIndex, offsetBy: 0)...self.cardNumberBuffer.index(self.cardNumberBuffer.startIndex, offsetBy: length-1)]
+            }
+            else
+            {
+                self.cardNumberBuffer = ""
+            }
+        }
+
+        textField.shouldChangeValue(cardNumberBuffer: self.cardNumberBuffer)
+        return false
+    }
 }
 
